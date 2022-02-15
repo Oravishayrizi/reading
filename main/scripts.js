d5ecded1c4636af750154b0d54787b0d5af481a5
Or Avishay-Rizi <30078665+Oravishayrizi@users.noreply.github.com>
2022-02-15 20:35:16 +0200

    Only VAV has Shuruk !!!

--- main/scripts.js
+++ main/scripts.js
@@ -1,20 +1,39 @@
 		function hardtoggle(){
 		var x = document.getElementById("btn_tip");
+		var y = document.getElementById("check9");
 		if (x.style.visibility === "visible") {
-		  x.style.visibility = "hidden";
+			x.style.visibility = "hidden";
+			y.disabled = false
 		} else {
 		  x.style.visibility = "visible";
+		  y.disabled = true
+		  y.checked = false
+
 		}
 		}
-	
+		
+		
+		function defualttoggle(){
+		var x = document.getElementById("btn_tip");
+			if (x.style.visibility != "visible") {
+				hardtoggle()
+		}
+		}
+
 	
 		function selects(){  
                 var ele=document.getElementsByName('pl');  
+				var x = document.getElementById("btn_tip");
+				var y = document.getElementById("check9");
                 for(var i=0; i<ele.length; i++){  
                     if(ele[i].type=='checkbox')  
                         ele[i].checked=true;  
-                }  
-            }  
+                }
+				if (x.style.visibility === "visible") {
+					y.checked = false;
+				}
+            }
+			
         function deSelect(){  
                 var ele=document.getElementsByName('pl');  
                 for(var i=0; i<ele.length; i++){  
@@ -22,7 +41,8 @@
                         ele[i].checked=false;  
                       
                 }  
-            }             
+            }
+		
 		function choosenikud() {
 		var markedCheckbox = document.getElementsByName('pl');
 		const checkedvar = [] ;
@@ -32,7 +52,7 @@
 		}
 		return(checkedvar);
 		}
-
+		
 		function images(rannum){
 		document.getElementById("myImg").src = "letters/"+rannum+".png";
         document.getElementById("mytip").src = "words/"+rannum+".png";
@@ -48,19 +68,44 @@
 			rannum1 = Math.floor(Math.random() * 22) + 1;
 		}
 		rannum2 = nikudall[Math.floor(Math.random() * nikudall.length)];
+		if	(rannum1 !=6) {
+		var i=0 ;
+		var stop= (i>10) | (rannum2 != 9);
+		while (stop==0) {
+			 i = i+1 ;
+			 rannum2 = nikudall[Math.floor(Math.random() * nikudall.length)];
+			 stop = (i>10) | (rannum2 != 9);
+		}
+		if	(i===11 & rannum2 == 9)  {
+		 var rannum2 = "error"
+		}
+		}
 		rannum = rannum1 + (rannum2-1)*22
 		images(rannum)
+
 		}
-		
+
 		function drawnikud(){
 		const nikudall = choosenikud()
 		x = document.getElementById("myImg").src.split(/letters\//).pop();
 		rannum1 = parseInt(x) % 22;
 		rannum2 = nikudall[Math.floor(Math.random() * nikudall.length)];
+		if	(rannum1 !=6) {
+		var i=0 ;
+		var stop= (i>10) | (rannum2 != 9);
+		while (stop==0) {
+			 i = i+1 ;
+			 rannum2 = nikudall[Math.floor(Math.random() * nikudall.length)];
+			 stop = (i>10) | (rannum2 != 9);
+		}
+		if	(i===11 & rannum2 == 9)  {
+		 var rannum2 = "error"
+		}
+		}
 		rannum = rannum1 + (rannum2-1)*22
 		images(rannum)
+
 		}
-		
 
 
 	    function showtip(){
